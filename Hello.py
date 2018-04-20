82071bd2d0dae3f0e4c039d50569ce2871fac8fc
William Keely <williamkeely@gmail.com>
2018-04-20 15:42:46 -0500

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,22 @@
+def file_get(fileName):
+   f = open(fileName, 'r')
+   text = f.read()
+   f.close()
+   return text
+
+def parse_text(text):
+    
+
+
+
+
+
+def main():
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
